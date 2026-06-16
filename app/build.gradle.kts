a0265ddfebb24520c99e7be6c6e61c1a7d5f4c3a
Helltar <iam@helltar.com>
2026-06-16 15:50:09 +0300

    bump version to 1.1 (versionCode 3) for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.helltar.catchrect"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
